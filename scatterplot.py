e51bb814d2af31293b75d99f26ade4b2088c1eae
NITHI2209 <nithyasree065@gmail.com>
2026-03-17 22:00:52 +0530

    created

---
+++ scatterplot.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+data = {
+    "salary": [12000,23000,33000,43000,43200,45000,45500,50000,55000,60000],
+   "Dept" : ["HR","Finance","IT","HR","IT","IT","HR","Finance","Finance","HR"],
+   "Age" : [33,34,45,50,55,65,70,70,75,80]
+}
+df = pd.DataFrame(data)
+plt.scatter(df["Age"],df["salary"],color = "yellow")
+plt.show()
+#Refer figure 8
