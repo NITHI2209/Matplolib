5a137d9e364b369b7605d7a0e42ec62c787b6336
NITHI2209 <nithyasree065@gmail.com>
2026-03-18 20:35:59 +0530

    created

---
+++ piechartNC.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+data = {
+    "salary": [12000,23000,33000,43000,43200,45000,45500,50000,55000,60000],
+   "Dept" : ["HR","Finance","IT","HR","IT","IT","HR","Finance","Finance","HR"],
+   "Age" : [33,34,45,50,55,65,70,70,75,80]
+}
+df = pd.DataFrame(data)
+salary_by_dept = df.groupby("Dept")["salary"].sum()
+plt.pie(salary_by_dept,labels=salary_by_dept.index,colors=["grey","pink","yellow"],autopct="%1.2f")
+plt.axis("equal")
+plt.show()
+#Refer figure 11
