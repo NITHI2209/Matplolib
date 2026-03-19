dcd151c664775d4c19feba30aec7ba8f4633003d
NITHI2209 <nithyasree065@gmail.com>
2026-03-19 14:00:55 +0530

    created

---
+++ 3Dprojection.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+data = {
+    "salary": [12000,23000,33000,43000,43200,45000,45500,50000,55000,60000],
+   "Dept" : ["HR","Finance","IT","HR","IT","IT","HR","Finance","Finance","HR"],
+   "Age" : [33,34,45,50,55,65,70,70,75,80]
+}
+df = pd.DataFrame(data)
+df["exprience"] = [1,1.5,1.7,1.8,2,2.5,3,3.5,4,5]
+ax = plt.axes(projection = "3d")
+ax.scatter(df["Age"],df["salary"],df["exprience"])
+ax.set_xlabel("Age")
+ax.set_ylabel("Salary")
+ax.set_zlabel("Exprience")
+plt.show()
+#Refer figure 16
